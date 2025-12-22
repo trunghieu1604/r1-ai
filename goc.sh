e6254917a1b53cf75689fc6fcfcf13323c171594
trunghieu1604 <trhieuforwork@gmail.com>
2025-12-22 15:08:21 +0700

    goc.sh

--- goc.sh
+++ goc.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-APK_NAME="Phicomm-R1.apk"
+APK_NAME="ai-box-plus.apk"
 APK_URL="https://github.com/trunghieu1604/r1-ai/releases/download/v1.0/$APK_NAME"
 APK_PATH="$HOME/$APK_NAME"
 ADB_DEVICE_IP="192.168.43.1"
